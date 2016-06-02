ef0f2fc2a1d3c57321c290209bd230457734dfff
supervarken <jorritspongebob@hotmail.com>
2016-06-02 16:56:46 +0200

    Update chat.js

--- public/chat.js
+++ public/chat.js
@@ -1,14 +1,15 @@
-$(document).ready(function () {
-
-
-      $('#jj').submit(function(){
-        socket.emit('chat message', $('#m').val());
-        $('#m').val('');
+document.getElementById('jj').addEventListener('submit', function(jj){
+    jj.preventDefault();
+       socket.emit('chat message', document.getElementById('m').value);
+        document.getElementById('m').value = "";
         return false;
-      });
-
+      })
+     
       socket.on('chat message', function(msg, name){
+       gameMsg = document.createElement("li");
+       
+		var node = document.createTextNode(name + ": " + msg);
+        gameMsg.appendChild(node);
 
-        $('#messages').append($('<li>').text(name + ": " + msg));
+        document.getElementById('messages').appendChild(gameMsg);
       });
-});
